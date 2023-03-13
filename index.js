2d1cff5a534ade5d0959f30331fc8cb3d1e56b3c
mohamedabdimohammud <mmohammud@outlook.com>
2023-03-13 14:49:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+    const button = document.getElementById("button");
+    function handleClick() {
+      console.log("Button clicked!");
+    }
+    button.addEventListener("click", handleClick);
+  }
+  // Get the button element
+const button = document.getElementById("button");
+
+// Define the event listener function
+function handleClick() {
+  console.log("Button clicked!");
 }
+
+// Bind the event listener to the button element
+button.addEventListener("click", handleClick);
